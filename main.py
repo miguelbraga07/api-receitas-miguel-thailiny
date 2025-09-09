acd28b154bf305b2aa58a0dcee0bbc23d8969776
thailinyanjos <thailiny.anjos05@aluno.ifce.edu.br>
2025-09-09 12:00:27 +0000

    alteraçao receita

--- main.py
+++ main.py
@@ -34,3 +34,31 @@ def create_receitas(dados:Receita):
 
    receitas.append(nova_receita)
    return nova_receita
+   
+   
+class CreateReceita(BaseModel):
+       nome: str
+       indredientes: List[str]
+       modo_de_preparo: str
+
+class Receitas(BaseModel):
+    id: int
+    nome: str
+    indredientes: List[str]
+    modo_de_preparo: str
+
+#nossa logica
+
+nova_receita = Receita(id= '''Nova ID''', nome='''nome que o usuario forneceu''')
+
+receitas = [
+    {"id": 1, "nome":"pudim"}
+]
+
+@app.get("/receitas/id/{id}")
+def get_receita_por_id():
+    for receita in receitas:
+        if receita["id"] == id:
+            return receita
+        return{"mensagem": "Receita não encontrada"}
+    
